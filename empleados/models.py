5f13bb0fb07d45460f01b663b70a3161f96693a3
Miguel Almonte <miguel.almonte1@gmail.com>
2017-03-08 17:17:45 -0400

    init

---
+++ empleados/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Empleado(models.Model):
+    cedula = models.TextField(max_length=20)
+    primer_nombre = models.TextField(max_length=150)
+    segundo_nombre = models.TextField(max_length=150)
+    primer_apellido = models.TextField(max_length=150)
+    segundo_apellido = models.TextField(max_length=150)
+
